a319cd02d73149d985408bffce4444dbd2cb4bd3
James Shin <jhscode@gmail.com>
2018-07-10 19:05:12 -0400

    install react-router-dom; create router.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './Header.js';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <div>App</div>
+        <Header />
       </div>
     );
   }
