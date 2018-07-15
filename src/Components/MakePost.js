a9bd3000c39a99fc9176d143f57e74fb3eee5816
James Shin <jhscode@gmail.com>
2018-07-15 17:47:27 -0400

    added back form for makepost

--- src/Components/MakePost.js
+++ src/Components/MakePost.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { Paper, Grid } from '@material-ui/core';
-
-const style = {
-  Paper: { padding: 20, marginTop: 10, marginBottom: 10 }
-};
 
 class MakePost extends React.Component {
   render() {
     return (
-      <Grid item xs={4}>
-        <Paper style={style.Paper}>Right</Paper>
-      </Grid>
+      <form>
+        <input type="text" placeholder="Title" />
+      </form>
     );
   }
 }
